62e0643e666ba4f5f2afa4355c35cf2fc94a7eaa
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-10-23 16:55:52 +0300

    ``` feat: Add @types/node dependency
    This commit adds the `@types/node` dependency to the
    project.  This is necessary to resolve type issues
    related to Node.js modules.  It also updates the
    `vite.config.ts` file to correctly resolve paths.
    ```

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import * as path from "node:path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/flappy-bird/',
